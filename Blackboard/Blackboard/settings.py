e7358ab69af22a23da002ac6702c34443344c849
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-04-09 00:13:20 +0330

    models_are_created

--- Blackboard/Blackboard/settings.py
+++ Blackboard/Blackboard/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "action"
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,15 @@ WSGI_APPLICATION = "Blackboard.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "USER" : "postgres",
+        "PASSWORD" : "69333032",
+        "NAME" : "Blackboard",
+        "HOST" : "localhost",
+        "PORT" :  5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
